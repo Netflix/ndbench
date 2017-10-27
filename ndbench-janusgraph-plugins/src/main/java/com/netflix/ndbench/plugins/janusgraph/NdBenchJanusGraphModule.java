b9fbd31f51f0d006d6abd4fbec2b3a7d7418c14e
cbedford <cbedford@netflix.com>
2017-10-26 17:31:35 -0700

    forgot to add annotation on newest client plugin

--- ndbench-janusgraph-plugins/src/main/java/com/netflix/ndbench/plugins/janusgraph/NdBenchJanusGraphModule.java
+++ ndbench-janusgraph-plugins/src/main/java/com/netflix/ndbench/plugins/janusgraph/NdBenchJanusGraphModule.java
@@ -3,12 +3,14 @@ package com.netflix.ndbench.plugins.janusgraph;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.netflix.archaius.ConfigProxyFactory;
+import com.netflix.ndbench.api.plugin.annotations.NdBenchClientPluginGuiceModule;
 import com.netflix.ndbench.plugins.janusgraph.configs.IJanusGraphConfig;
 import com.netflix.ndbench.plugins.janusgraph.configs.cql.ICQLConfig;
 
 /**
  * @author pencal
  */
+@NdBenchClientPluginGuiceModule
 public class NdBenchJanusGraphModule extends AbstractModule {
 
     @Override
