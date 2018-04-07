fb98846b2c6cd9efc62551efc0960ebba8a0ed6a
Alexander Patrikalakis <amcp@amazon.co.jp>
2018-04-06 22:11:44 -0700

    Wires DynamoDB configuration options (gzip, maxconn, region, endpoint)
    Fixes #113

--- ndbench-dynamodb-plugins/src/main/java/com/netflix/ndbench/plugin/dynamodb/configs/DynamoDBConfigs.java
+++ ndbench-dynamodb-plugins/src/main/java/com/netflix/ndbench/plugin/dynamodb/configs/DynamoDBConfigs.java
@@ -30,7 +30,7 @@ import com.netflix.ndbench.api.plugin.common.NdBenchConstants;
 @Configuration(prefix =  NdBenchConstants.PROP_NAMESPACE +  "dynamodb")
 public interface DynamoDBConfigs {
 
-        @PropertyName(name = "tablename")
+	@PropertyName(name = "tablename")
 	@DefaultValue("ndbench-table")
 	String getTableName();
 
@@ -62,6 +62,35 @@ public interface DynamoDBConfigs {
 	@PropertyName(name = "consistentread")
 	@DefaultValue("false")
 	Boolean consistentRead();
+
+	/*
+	 * Compression: HTTP clients for DynamoDB can be configured to use GZip compression.
+	 * Effects are negligible for small items, but can be significant for large items with
+	 * high deflation ratios.
+	 */
+	@PropertyName(name = "compressing")
+	@DefaultValue("false")
+	Boolean isCompressing();
+
+	/*
+	 * Region: Allowing customers to override the region enables baselining cross-region use cases
+	 */
+	@DefaultValue("us-east-1")
+	String getRegion();
+
+	/*
+	 * Region: Allowing customers to override the endpoint enables baselining cross-region use cases
+	 * and testing with DynamoDB local
+	 */
+	@DefaultValue("https://dynamodb.us-east-1.amazonaws.com")
+	String getEndpoint();
+
+	/*
+	 * Max connections: the HTTP client in the DynamoDB client has a connection pool. Making it configurable here
+	 * makes it possible to drive workloads from one host that require more than 50 total read and write workers.
+	 */
+	@DefaultValue("50")
+	Integer getMaxConnections();
 	
 	/*
 	 * This configuration allows to create a table programmatically (through NDBench).
