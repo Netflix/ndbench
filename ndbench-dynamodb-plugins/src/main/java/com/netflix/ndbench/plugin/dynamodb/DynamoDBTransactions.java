204dab826f8aa72fe7a3938fb34ae96a13df763a
sumanthpasupuleti <spasupuleti@netflix.com>
2019-01-07 11:12:49 -0800

    Removing programmatic way of creating and deleting tables, since it results in race condition failures among ndbench clients and console is an easier alternative to do such admin operations

--- ndbench-dynamodb-plugins/src/main/java/com/netflix/ndbench/plugin/dynamodb/DynamoDBTransactions.java
+++ ndbench-dynamodb-plugins/src/main/java/com/netflix/ndbench/plugin/dynamodb/DynamoDBTransactions.java
@@ -15,23 +15,12 @@
  */
 package com.netflix.ndbench.plugin.dynamodb;
 
-import java.util.ArrayList;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.services.dynamodbv2.model.DescribeLimitsResult;
-import com.amazonaws.services.dynamodbv2.model.TableDescription;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.netflix.ndbench.api.plugin.DataGenerator;
 import com.netflix.ndbench.api.plugin.NdBenchClient;
 import com.netflix.ndbench.api.plugin.annotations.NdBenchClientPlugin;
-import com.netflix.ndbench.plugin.dynamodb.configs.ProgrammaticDynamoDBConfiguration;
-import com.netflix.ndbench.plugin.dynamodb.operations.dynamodb.controlplane.CreateDynamoDBTable;
-import com.netflix.ndbench.plugin.dynamodb.operations.dynamodb.controlplane.DeleteDynamoDBTable;
-import com.netflix.ndbench.plugin.dynamodb.operations.dynamodb.controlplane.DescribeLimits;
+import com.netflix.ndbench.plugin.dynamodb.configs.DynamoDBConfiguration;
 
 /**
  * This NDBench plugin uses DynamoDB's transaction API to write to multiple tables
@@ -41,11 +30,7 @@ import com.netflix.ndbench.plugin.dynamodb.operations.dynamodb.controlplane.Desc
  */
 @Singleton
 @NdBenchClientPlugin("DynamoDBTransactions")
-public class DynamoDBTransactions extends DynamoDBKeyValueBase<ProgrammaticDynamoDBConfiguration> implements NdBenchClient {
-    private static final Logger logger = LoggerFactory.getLogger(DynamoDBTransactions.class);
-
-    private DynamoDBAutoscalingConfigurer dynamoDBAutoscalingConfigurer;
-    private ArrayList<DeleteDynamoDBTable> deleteTables;
+public class DynamoDBTransactions extends DynamoDBKeyValue {
 
     /**
      * Public constructor to inject credentials and configuration
@@ -54,74 +39,8 @@ public class DynamoDBTransactions extends DynamoDBKeyValueBase<ProgrammaticDynam
      * @param configuration
      */
     @Inject
-    public DynamoDBTransactions(AWSCredentialsProvider awsCredentialsProvider,
-                                ProgrammaticDynamoDBConfiguration configuration,
-                                DynamoDBAutoscalingConfigurer dynamoDBAutoscalingConfigurer) {
+    public DynamoDBTransactions(AWSCredentialsProvider awsCredentialsProvider, DynamoDBConfiguration configuration) {
         super(awsCredentialsProvider, configuration);
-        this.dynamoDBAutoscalingConfigurer = dynamoDBAutoscalingConfigurer;
-    }
-
-    @Override
-    public void init(DataGenerator dataGenerator) {
-        createAndSetDynamoDBClient();
-        instantiateDataPlaneOperations(dataGenerator);
-
-        // prerequisite data from configuration
-        String tableName = config.getTableName();
-        String partitionKeyName = config.getAttributeName();
-        long rcu = Long.parseLong(config.getReadCapacityUnits());
-        long wcu = Long.parseLong(config.getWriteCapacityUnits());
-
-        this.deleteTables = new ArrayList<>();
-        // create main table object
-        CreateDynamoDBTable mainTable = new CreateDynamoDBTable(dynamoDB, tableName, partitionKeyName, rcu, wcu);
-        this.deleteTables.add(new DeleteDynamoDBTable(dynamoDB, tableName, partitionKeyName));
-
-        // create child tables objects
-        ArrayList<CreateDynamoDBTable> childTables = new ArrayList<>();
-        for (int i = 0; i < config.getMainTableColsPerRow(); i++)
-        {
-            String childTableName = config.getChildTableNamePrefix() + i;
-            childTables.add(new CreateDynamoDBTable(dynamoDB, childTableName, partitionKeyName, rcu, wcu));
-            deleteTables.add(new DeleteDynamoDBTable(dynamoDB, childTableName, partitionKeyName));
-        }
-
-        // execute main table creation
-        logger.info("Creating main table programmatically");
-        TableDescription td = mainTable.get();
-        logger.info("Main Table Description: " + td.toString());
-
-        // execute child tables creation
-        logger.info("Creating child table programmatically");
-        for (int i = 0; i < config.getMainTableColsPerRow(); i++)
-        {
-            td = childTables.get(i).get();
-            logger.info("Child Table Description: " + td.toString());
-        }
-
-        DescribeLimits describeLimits = new DescribeLimits(dynamoDB, tableName, partitionKeyName);
-        final DescribeLimitsResult limits = describeLimits.get();
-        if (config.getAutoscaling()) {
-            // main table
-            dynamoDBAutoscalingConfigurer.setupAutoscaling(rcu, wcu, config.getTableName(), limits,
-                    Integer.valueOf(config.getTargetWriteUtilization()),
-                    Integer.valueOf(config.getTargetReadUtilization()));
-
-            // child tables
-            for (int i = 0; i < config.getMainTableColsPerRow(); i++)
-            {
-                dynamoDBAutoscalingConfigurer.setupAutoscaling(rcu, wcu, config.getChildTableNamePrefix() + i, limits,
-                                                               Integer.valueOf(config.getTargetWriteUtilization()),
-                                                               Integer.valueOf(config.getTargetReadUtilization()));
-            }
-        }
-    }
-
-    @Override
-    public String readSingle(String key)
-    {
-        // read from main table
-        return singleRead.apply(key);
     }
 
     /**
@@ -134,11 +53,5 @@ public class DynamoDBTransactions extends DynamoDBKeyValueBase<ProgrammaticDynam
     public String writeSingle(String key)
     {
         return transactionWrite.apply(key);
-    }
-
-    @Override
-    public void shutdown() {
-        super.shutdown();
-        deleteTables.forEach(deleteTable -> deleteTable.delete());
     }
 }
